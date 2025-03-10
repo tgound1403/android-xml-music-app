c1dde406c4e5003cc5ff8b362b2da0b286e194dd
ntrieuduong <ntrieuduong@tma.com.vn>
2025-03-10 15:47:14 +0700

    code after final rkt

--- app/src/main/java/com/tmatraining/musicapp/MyApplication.kt
+++ app/src/main/java/com/tmatraining/musicapp/MyApplication.kt
@@ -1,17 +1,8 @@
 package com.tmatraining.musicapp
 
-import android.app.Activity
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication : Application() {}
 
-fun Context.getAppInstance(): MyApplication {
-    return when (this) {
-        is MyApplication -> this
-        is Activity -> application as MyApplication
-        else -> applicationContext as MyApplication
-    }
-}
